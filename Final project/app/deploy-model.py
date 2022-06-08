428a12486b2f4c51ec33db1266cb8566e7558255
Patricia Sanz <patricia.sanz.escurin@gmail.com>
2022-06-08 18:33:14 +0200

    .

--- Final project/app/deploy-model.py
+++ Final project/app/deploy-model.py
@@ -1,81 +1,31 @@
 import streamlit as st
 import pandas as pd
-import shap
-import matplotlib.pyplot as plt
-from sklearn import datasets
-from sklearn.ensemble import RandomForestRegressor
+import pickle as pkl
+
+
+model = pkl.load(open('../models/model.pkl', 'rb'))
 
 st.write("""
-# Prediction App
-This app predicts the **House Price**!
+# Predicción del precio de la casa
 """)
-st.write('---')
 
-# Loads the House Price Dataset
-houses = datasets.load_boston()
-X = pd.DataFrame(houses.data, columns=houses.feature_names)
-Y = pd.DataFrame(houses.target, columns=["MEDV"])
+bedrooms = st.number_input('Introduzca el número de habitaciones', value=0)
+bathrooms = st.number_input('Introduzca el número de baños', value=0)
+floor = st.number_input('Introduzca el número de pisos', value=0)
+sqft_living = st.number_input('Introduzca el número de metros cuadrados', value=0)
+    
 
-# Sidebar
-# Header of Specify Input Parameters
-st.sidebar.header('Input Parameters')
+if st.button('Calcular'):
+    data = pd.DataFrame({'bedrooms': [bedrooms], 'bathrooms': [bathrooms],
+            'floor': [floor],
+            'sqft_living': [sqft_living]})
 
-def user_input_features():
-    bed = st.sidebar.slider('bedroom', X.bed.min(), X.bed.max(), X.bed.mean())
-    bath = st.sidebar.slider('bathroom', X.bath.min(), X.bath.max(), X.bath.mean())
-    floor = st.sidebar.slider('floor', X.floor.min(), X.floor.max(), X.floor.mean())
-    wf = st.sidebar.slider('waterfront', X.wf.min(), X.wf.max(), X.wf.mean())
-    view = st.sidebar.slider('view', X.view.min(), X.view.max(), X.view.mean())
-    cond = st.sidebar.slider('conditions', X.cond.min(), X.cond.max(), X.cond.mean())
-    grade = st.sidebar.slider('grade', X.grade.min(), X.grade.max(), X.grade.mean())
-    price = st.sidebar.slider('price', X.price.min(), X.price.max(), X.price.mean())
-    code = st.sidebar.slider('zipcode', X.code.min(), X.code.max(), X.code.mean())
-    lat = st.sidebar.slider('lat', X.lat.min(), X.lat.max(), X.lat.mean())
-    long = st.sidebar.slider('long', X.long.min(), X.long.max(), X.long.mean())
-    data = {'bedrooms': bed,
-            'bathroom': bath,
-            'floor': INDUS,
-            'waterfront': wf,
-            'view': view,
-            'conditions': cond,
-            'grade': grade,
-            'price': price,
-            'zipcode': code,
-            'lat': lat,
-            'long': long }
-    features = pd.DataFrame(data, index=[0])
-    return features
 
-df = user_input_features()
+    data
 
-# Main Panel
+    prediction = model.predict([[sqft_living, bathrooms+floor+bedrooms]])
 
-# Print specified input parameters
-st.header('Parameters')
-st.write(df)
-st.write('---')
+    st.success('La casa costará {} euros'.format(int(prediction[0])))
 
-# Build Regression Model
-model = RandomForestRegressor()
-model.fit(X, Y)
-# Apply Model to Make Prediction
-prediction = model.predict(df)
 
-st.header('Prediction')
-st.write(prediction)
-st.write('---')
-
-# Explaining the model's predictions using SHAP values
-# https://github.com/slundberg/shap
-explainer = shap.TreeExplainer(model)
-shap_values = explainer.shap_values(X)
-
-st.header('Feature Importance')
-plt.title('Feature importance based on SHAP values')
-shap.summary_plot(shap_values, X)
-st.pyplot(bbox_inches='tight')
-st.write('---')
-
-plt.title('Feature importance based on SHAP values (Bar)')
-shap.summary_plot(shap_values, X, plot_type="bar")
-st.pyplot(bbox_inches='tight')
+           
